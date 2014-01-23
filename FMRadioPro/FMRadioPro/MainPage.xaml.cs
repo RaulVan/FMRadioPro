2664d7b0b75eebdf022036cd272774207f7f5d19
范智灿 <zhican.fan@autonavi.com>
2014-01-23 16:58:30 +0800

    Add new project

---
+++ FMRadioPro/FMRadioPro/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using FMRadioPro.Resources;
+
+namespace FMRadioPro
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+
+       // AudioCategory 
+        // 构造函数
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // 用于本地化 ApplicationBar 的示例代码
+            //BuildLocalizedApplicationBar();
+            List<string> data=new List<string> ();
+            for (int i = 0; i < 100; i++)
+			{
+                data.Add(i + "/Deanna 频道 test频道 test频道 test频道 test");
+			}
+            listRadioList.ItemsSource = data;
+           // gridPanel.Width = Application.Current.Host.Content.ActualWidth * 2;
+
+           
+        }
+
+      
+
+        // 用于生成本地化 ApplicationBar 的示例代码
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // 将页面的 ApplicationBar 设置为 ApplicationBar 的新实例。
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // 创建新按钮并将文本值设置为 AppResources 中的本地化字符串。
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // 使用 AppResources 中的本地化字符串创建新菜单项。
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,9 @@ FMRadioPro
 ==========
 
 FM Radio Pro for Windows Phone 
+
+开发日志
+==========
+2014/1/23
+1、新建项目
+2、设计界面
