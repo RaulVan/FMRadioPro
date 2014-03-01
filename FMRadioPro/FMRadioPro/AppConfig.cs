a667d3cc4354f91cf957429e4311647d448d1611
RaulVan <vanraul@outlook.com>
2014-03-01 18:17:08 +0800

    add AppConfig.cs

---
+++ FMRadioPro/FMRadioPro/AppConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMRadioPro
+{
+   public  class AppConfig
+    {
+       /// <summary>
+        /// 当前播放曲目
+       /// </summary>
+       public static int isoCurrentTrack
+       {
+           get
+           {
+               return IsolatedStorageSettings.ApplicationSettings.Contains("isoCurrentTrack") ? (int)IsolatedStorageSettings.ApplicationSettings["isoCurrentTrack"] : 0;
+           }
+           set
+           {
+               IsolatedStorageSettings.ApplicationSettings["isoCurrentTrack"] = value;
+               IsolatedStorageSettings.ApplicationSettings.Save();
+           }
+       }
+
+    }
+}
