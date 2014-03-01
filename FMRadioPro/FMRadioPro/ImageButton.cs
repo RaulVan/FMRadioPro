d102a96df65518727ed7ed9ce9eecf5d2f1cda7f
RaulVan <vanraul@outlook.com>
2014-03-01 19:11:35 +0800

    修改播放暂停按钮，使用圆形ImageButton

---
+++ FMRadioPro/FMRadioPro/ImageButton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace FMRadioPro
+{
+    public class ImageButton :Button
+    {
+        public static readonly DependencyProperty ImageSourceProperty =
+        DependencyProperty.Register(
+            "ImageSource",
+            typeof(ImageSource),
+            typeof(ImageButton),
+            null);
+
+        public ImageSource ImageSource
+        {
+            get { return (ImageSource)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+       
+    }
+}
