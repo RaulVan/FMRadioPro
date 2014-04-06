54602dec8571c9ac3fd4c9c76706585544830662
RaulVan <vanraul@outlook.com>
2014-04-06 21:59:17 +0800

    FM used loopSelector

--- FMRadioPro/FMRadioPro/FMPage.xaml.cs
+++ FMRadioPro/FMRadioPro/FMPage.xaml.cs
@@ -10,33 +10,39 @@ using Microsoft.Phone.Shell;
 using Microsoft.Devices.Radio;
 using System.Windows.Threading;
 using System.Diagnostics;
+using FMRadioPro.Utilities;
 
 namespace FMRadioPro
 {
     public partial class FMPage : PhoneApplicationPage
     {
+        private string strFrequency;
+
         FMRadio fmRadio;
         public FMPage()
         {
             InitializeComponent();
+            this.loopSelector1.DataSource = new IntLoopingDataSource() { MinValue = 87, MaxValue = 108, SelectedItem = 87 };
+            this.loopSelector2.DataSource = new IntLoopingDataSource() { MinValue = 0, MaxValue = 10, SelectedItem = 1 };
+
             btnPlay.Click += btnPlay_Click;
             this.Loaded += FMPage_Loaded;
         }
 
         void FMPage_Loaded(object sender, RoutedEventArgs e)
         {
-            fmRadio = FMRadio.Instance;
-            fmRadio.PowerMode = RadioPowerMode.On;
+            //fmRadio = FMRadio.Instance;
+            //fmRadio.PowerMode = RadioPowerMode.On;
         }
 
         void btnPlay_Click(object sender, RoutedEventArgs e)
         {
-            DispatcherTimer timer = new DispatcherTimer();
-            timer.Tick += (a,s) =>
-                {
-                    Debug.WriteLine(fmRadio.SignalStrength.ToString());
+            //DispatcherTimer timer = new DispatcherTimer();
+            //timer.Tick += (a,s) =>
+            //    {
+            //        Debug.WriteLine(fmRadio.SignalStrength.ToString());
 
-                };
+            //    };
 
         }
 
